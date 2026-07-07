ef0dcb3d2d0121548e2be18c8b4ce258cfcbe167
christianhm97 <christian.hidalgo.martin@hotmail.com>
2026-07-07 12:56:23 +0200

    Tareas módulo 3.1

--- actividad09/src/main/java/modulo3_1_actividad09/Actividad09.java
+++ actividad09/src/main/java/modulo3_1_actividad09/Actividad09.java
@@ -3,5 +3,11 @@ package modulo3_1_actividad09;
 public class Actividad09 {
     public static void main(String[] args) throws Exception {
         // TODO: declara las variables de la pelicula y muestra el resumen.
+    	String nombre = "Matrix";
+    	int duracion = 136;
+    	double puntuacion = 8.7;
+    	boolean recomendada = true;
+    	
+    	System.out.println(nombre + " dura " + duracion + " minutos. Puntuacion: " + puntuacion+ ". Recomendada: " + recomendada);
     }
 }
