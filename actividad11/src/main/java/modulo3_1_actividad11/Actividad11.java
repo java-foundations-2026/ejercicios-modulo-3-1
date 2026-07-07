26b6b75d1c4e08eb88af4b3b71aba441274902e5
RelatingDarkX <ivacon40@hotmail.com>
2026-07-07 13:04:15 +0200

    Tareas mod 3-1

--- actividad11/src/main/java/modulo3_1_actividad11/Actividad11.java
+++ actividad11/src/main/java/modulo3_1_actividad11/Actividad11.java
@@ -6,11 +6,15 @@ public class Actividad11 {
         double nota1 = 7.0;
         double nota2 = 8.0;
         double nota3 = 9.0;
+        double resultado = nota1 + nota2 + nota3;
+        double total = resultado / 3;
         boolean asistencia = true;
 
         // TODO: calcula el promedio y muestra el informe.
     	System.out.println("Nombre: "+ nombre);
     	System.out.println("El promedio de las notas es: "+ nombre);
+    	System.out.println("Promedio: "+ total);
+    	System.out.println("Asistencia "+asistencia);
 
 
     }
